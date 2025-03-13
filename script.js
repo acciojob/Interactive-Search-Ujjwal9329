b04621bca619b596787a8b6d30344026d24deb2a
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-03-13 16:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const searchDiv = document.querySelector(".search");
+const inputField = document.querySelector(".input");
+const searchBtn = document.querySelector(".btn");
+
+searchBtn.addEventListener("click", () => {
+    searchDiv.classList.add("active");
+    inputField.focus();
+});
